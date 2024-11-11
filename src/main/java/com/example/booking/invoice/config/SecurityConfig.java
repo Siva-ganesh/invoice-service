3afd00fb71ba24e4b011c575594386cb5769b92c
Siva-ganesh <209899@ibsplc.com>
2024-11-11 12:09:32 +0530

    OAuth config code changes

--- src/main/java/com/example/booking/invoice/config/SecurityConfig.java
+++ src/main/java/com/example/booking/invoice/config/SecurityConfig.java
@@ -17,14 +17,17 @@ public class SecurityConfig {
                 .authorizeHttpRequests(authorizeRequests ->
                         authorizeRequests
                                 .anyRequest().authenticated()
+                ) .oauth2Login(oauth2Login -> oauth2Login
+                        .defaultSuccessUrl("/index.html", true) // Redirect to /index.html page on successful login
                 );
+
         return http.build();
     }
 
     @Bean
     WebSecurityCustomizer ignoringCustomizer() {
         return web -> web.ignoring().requestMatchers(
-                "/h2-console/**", "/api/invoice/save", "/actuator/**"
+                "/h2-console/**", "/api/invoice/save"
         );
     }
 }
